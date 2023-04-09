8ae8017365ed6237327cc9af85838b6e677bf58b
SGCodes01 <codes.sg@gmail.com>
2023-04-10 01:11:31 +0530

    Added Login & Signup Design

---
+++ App.js
@@ -0,0 +1,22 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/Login';
+import Signup from './src/Signup';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
